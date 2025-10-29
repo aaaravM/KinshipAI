3da1f7dbe613944253f725669d93bd4427499098
Aarav Muttanapalli <muttanapalliAarav26@gmail.com>
2025-10-28 18:19:43 -0700

    Initial Commit

---
+++ KinshipAI/KinshipAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  KinshipAIApp.swift
+//  KinshipAI
+//
+//  Created by Sheetal Rani Muttanapalli on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct KinshipAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
